4c9e65fe29e36f94386cc3554cb9c51af507904d
Sumit99089 <sumitlaha47@gmail.com>
2025-10-20 14:41:31 +0530

    Fixed Minor Bug

--- app/src/main/java/com/example/music/MainActivity.kt
+++ app/src/main/java/com/example/music/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.music
 
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,8 +9,17 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.music.domain.models.AlbumModel
+import com.example.music.domain.models.ArtistModel
+import com.example.music.domain.models.SongModel
 import com.example.music.infrastructure.service.MusicServiceConnection
 import com.example.music.presentation.library.LibraryScreen
+import com.example.music.presentation.library.LibraryScreenContent
+import com.example.music.presentation.library.LibraryState
+import com.example.music.presentation.library.Tab
+import com.example.music.presentation.miniplayer.MiniPlayer
 import com.example.music.presentation.permissions.PermissionHandler
 import com.example.music.ui.theme.MusicTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,21 +31,11 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var musicServiceConnection: MusicServiceConnection
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MusicTheme {
-                PermissionHandler {
-                    // This content will only be shown if permissions are granted
-                    Scaffold(
-                        topBar = {
-                            TopAppBar(title = { Text("Music") })
-                        }
-                    ) { innerPadding ->
-                        LibraryScreen(paddingValues = innerPadding)
-                    }
-                }
+
             }
         }
     }
@@ -45,5 +45,24 @@ class MainActivity : ComponentActivity() {
         musicServiceConnection.release()
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MusicAppContent() {
+    PermissionHandler {
+        // This content will only be shown if permissions are granted
+        Scaffold(
+            topBar = {
+                TopAppBar(title = { Text("Music") })
+            },
+            bottomBar = {
+                MiniPlayer()
+            }
+        ) { innerPadding ->
+            LibraryScreen(paddingValues = innerPadding)
+        }
+    }
+}
+
 
 
